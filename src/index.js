7784d5a9ada1daefa3a5bb52a4ca3aacebb9fd05
kiri-91 <kirifog91@gmail.com>
2022-10-14 11:16:22 +0900

    create js

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+// SquareクラスをReact.Componentクラスから作成する。
+class Square extends React.Component {
+	// 状態によってUIを更新する
+	render() {
+		// JSXを返す
+		return (
+			<button className='square'>
+				{/* TODO */}
+			</button>
+		);
+	}
+}
+
+class Board extends React.Component {
+	renderSquare(i) {
+		return <Square />;
+	}
+
+	render() {
+		const status = "Next player: X";
+
+		return (
+			<div>
+				<div className='status'>{status}</div>
+				<div className='board-row'>
+					{this.renderSquare(0)}
+					{this.renderSquare(1)}
+					{this.renderSquare(2)}
+				</div>
+				<div className='board-row'>
+					{this.renderSquare(3)}
+					{this.renderSquare(4)}
+					{this.renderSquare(5)}
+				</div>
+				<div className='board-row'>
+					{this.renderSquare(6)}
+					{this.renderSquare(7)}
+					{this.renderSquare(8)}
+				</div>
+			</div>
+		);
+	}
+}
+
+class Game extends React.Component {
+	render() {
+		return (
+			<div className='game'>
+				<div className='game-board'>
+					<Board />
+				</div>
+				<div className='game-info'>
+					<div>{/* status */}</div>
+					<div>{/* TODO */}</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
